82c060862f7dab7cdf2ac19af238f36143dbbac7
Lexan92 <luis_grennby92@hotmail.com>
2016-04-20 12:53:21 -0600

    Initial commit

---
+++ app/src/main/java/clase09/fia/ues/sv/basedatoscarnet/AlumnoEliminarActivity.java
@@ -0,0 +1,13 @@
+package clase09.fia.ues.sv.basedatoscarnet;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlumnoEliminarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alumno_eliminar);
+    }
+}
---
+++ app/src/main/java/clase09/fia/ues/sv/basedatoscarnet/Materia.java
@@ -0,0 +1,7 @@
+package clase09.fia.ues.sv.basedatoscarnet;
+
+/**
+ * Created by Alex on 20/4/2016.
+ */
+public class Materia {
+}
