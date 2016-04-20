ff1a943bedf1fbdad6ec84d7f2c93ab7ad30066b
Lexan92 <luis_grennby92@hotmail.com>
2016-04-20 13:00:18 -0600

    clase activity

--- app/src/main/java/clase09/fia/ues/sv/basedatoscarnet/BaseDatosCarnetActivity.java
+++ app/src/main/java/clase09/fia/ues/sv/basedatoscarnet/BaseDatosCarnetActivity.java
@@ -5,9 +5,29 @@ import android.os.Bundle;
 
 public class BaseDatosCarnetActivity extends AppCompatActivity {
 
+    String[] menu={"Tabla Alumno","Tabla Nota","Tabla Materia","LLenar Base de Datos"};
+    String[] activities={"AlumnoMenuActivity","NotaMenuActivity","MateriaMenuActivity"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base_datos_carnet);
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu));
     }
+    @Override
+    protected void onListItemClick(ListView l,View v,int position,long id){
+        super.onListItemClick(l, v, position, id);
+        if(position!=3){
+            String nombreValue=activities[position];
+            try{
+                Class<?> clase=Class.forName("sv.ues.fia.basedatoscarnet."+nombreValue);
+                Intent inte = new Intent(this,clase);
+                this.startActivity(inte);
+            }catch(ClassNotFoundException e){
+                e.printStackTrace();
+            }
+        }else{
+//CODIGO PARA LLENAR BASE DE DATOS
+        }
+    }
+
 }
